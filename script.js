1a384cacc2c870221513840e729b9c8a53156d20
mankyb0mb <rainerb0716@gmail.com>
2024-06-17 21:08:38 -0700

    asdfasdf

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let gridHolder = document.querySelector(".gridHolder")
-
-let grids = 64
+let changer = document.querySelector("#changer")
 
 let gridRow = document.createElement("div")
 gridRow.style.display = "flex"
@@ -16,26 +15,38 @@ gridFill.style.fontSize = "8px"
 gridFill.classList = ""
 gridFill.style.backgroundColor = "azure"
 
-//creates the Rows
-for (let i = 0; i < grids; i++) {
-    let gRow = gridRow.cloneNode()
-    gRow.classList = "row" + (i + 1)
-    gRow.textContent = ""
-    gridHolder.appendChild(gRow)
-}
+changer.addEventListener("click", () => {
+    let grids = prompt("How many? (Up to 100)")
+    yapper(grids)
+    
+})
 
-//fills in the Rows
-for (let i = 0; i < grids; i++) {
-    let querySelector = ".row" + (i + 1)
-    let rows = document.querySelector(querySelector)
-    for (let j = 0; j < grids; j++) {
-        let gFill = gridFill.cloneNode()
-        gFill.classList = "Grid-" + (i+1) + "-" + (j+1)
-
-        gFill.textContent = j + 1
-        // gFill.textContent = i * grids + j + 1
-
-        rows.appendChild(gFill)
+function yapper(grids = 16){
+    gridHolder.innerHTML = ""
+    //creates the Rows
+    for (let i = 0; i < grids; i++) {
+        let gRow = gridRow.cloneNode()
+        gRow.classList = "row" + (i + 1)
+        gRow.textContent = ""
+        gridHolder.appendChild(gRow)
     }
 
+    //fills in the Rows
+    for (let i = 0; i < grids; i++) {
+        let querySelector = ".row" + (i + 1)
+        let rows = document.querySelector(querySelector)
+        for (let j = 0; j < grids; j++) {
+            let gFill = gridFill.cloneNode()
+            gFill.classList = "Grid-" + (i+1) + "-" + (j+1)
+
+            // gFill.textContent = j + 1
+            // gFill.textContent = i * grids + j + 1
+
+            rows.appendChild(gFill)
+        }
+
+    }
 }
+
+yapper()
+
